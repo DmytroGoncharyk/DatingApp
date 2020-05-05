a8b1d35e4d386a7f1bcada483fbb7f176eb9ab82
Dmytro Goncharyk <55553333d@gmail.com>
2020-05-05 13:30:43 -0500

    End of Section 7

---
+++ DatingApp/DatingApp-SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,35 @@
+import { AlertifyService } from './../_services/alertify.service';
+import { AuthService } from './../_services/auth.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.authService.login(this.model).subscribe(next => {
+      this.alertify.success('logged in successfuly');
+    }, error => {
+      this.alertify.error(error);
+    });
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.alertify.message('logged out');
+  }
+}
